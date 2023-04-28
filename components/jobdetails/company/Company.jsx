1c49da85482b0e226f0392d8fc6e42920dc547db
Josh Koter <josh.koter@gmail.com>
2023-04-28 20:07:18 +0200

    Created skeleton and some components
    The .env import is not working. It is commented out for now.

---
+++ components/jobdetails/company/Company.jsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+
+import styles from './company.style'
+
+const Company = () => {
+  return (
+    <View>
+      <Text>Company</Text>
+    </View>
+  )
+}
+
+export default Company
